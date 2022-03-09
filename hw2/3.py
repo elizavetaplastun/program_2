a25ecde40a08e675f91ae6a5e115afb1c628781f
kponomareva2 <2016.fox.katya@gmail.com>
2022-03-09 23:58:55 +0300

    hw2

---
+++ hw2/3.py
@@ -0,0 +1,35 @@
+from random import randint
+from tabulate import tabulate
+
+
+def expectationSearch(array):
+    # expected_value = probability * sum(array)
+    expected_value = 0
+    for x in array:
+        expected_value += probability * x
+    return expected_value
+
+
+def dispersionSearch(array, expected_value):
+    expected_value_new = 0
+    for x in array:
+        expected_value_new += x**2 * probability
+    dispersion = expected_value_new - expected_value**2
+    return dispersion
+
+
+print('Enter number of rows and columns(n): ')
+n = int(input())
+probability = 1/n
+randomVariables = [[randint(1, 30) for j in range(n)] for i in range(n)]
+print('resulting table:')
+print(tabulate(randomVariables))
+
+for i in range(n):
+    print(f'{i+1} row:\nexpected value =', expectationSearch(randomVariables[i]),
+          '\ndispersion =', dispersionSearch(randomVariables[i], expectationSearch(randomVariables[i])))
+
+# print('Expected value and dispersion in which row do you want to count?')
+# row = int(input())
+# print('expected value =', expectationSearch(randomVariables[row-1]),
+#       '\ndispersion =', dispersionSearch(randomVariables[row-1], expectationSearch(randomVariables[row-1])))
