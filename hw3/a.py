172c73a261c5c0c6b2283b5bf2bd5ad3a9063784
elizavetaplastun <lizaplastun@gmail.com>
2022-03-23 23:12:08 +0300

    a

--- hw3/a.py
+++ hw3/a.py
@@ -1,14 +1,24 @@
+import requests
 
-from urllib.request import urlopen
-
-companies = {"Газпром": [[89, 678], [7, 900]], "Татнефть": [], "Сбербанк": [], "ВТБ": [], "Алроса": [], "Аэрофлот": [],
-             "РусГидро": [], "Московская Биржа": [], "НЛМК": [], "Северсталь": [], "Детский Мир": [], "Полиметалл": [],
-             "Яндекс": [], "АФК": [],
-             "Система": [], "Группа ЛСР": [], "Ленэнерго": [], "Лукойл": [], "МТС": [], "Новатэк": [], "ПИК": []}
-
-company = "GAZP"
-data = "2016-01-01"
-url = f"http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/{company}.json?iss.json=extended&from={data}"
-text = str(urlopen(url).read())
-t = text.split()
-print(t)
+companies = {"GAZP": [[], []], "TATN": [[], []], "SBER": [[], []], "VTBR": [[], []], "ALRS": [[], []],
+             "AFLT": [[], []],
+             "AFLT": [[], []], "MOEX": [[], []], "NLMK": [[], []], "CHMF": [[], []],
+             "DSKY": [[], []], "POLY": [[], []],
+             "YNDX": [[], []], "AFKS": [[], []],
+             "LSRG": [[], []], "LSNG": [[], []], "LKOH": [[], []], "MTSS": [[], []],
+             "NVTK": [[], []], "PIKK": [[], []]}
+date_list = ["01-01-2016", "01-07-2016", "01-01-2017", "01-07-2017", "01-01-2018", "01-01-2019", "01-07-2019"]
+url = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json?iss.meta=off&marketdata.columns=SECID,ISSUECAPITALIZATION&securities.columns=SECID,SHORTNAME,ISSUESIZE"
+data = requests.get(url).json()["securities"]["data"]
+for i in data:
+    if i[0] in companies.keys():
+        companies[i[0]][0] = int(i[2])
+for company in companies.keys():
+    data1 = "01-01-2016"
+    data2 = "31-12-2019"
+    url2 = f"https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/{company}.json?from={data1}&till={data2}&history.columns=TRADEDATE,SHORTNAME,OPEN&iss.meta=off"
+    data = requests.get(url2).json()["history"]["data"]
+    for day in data:
+        if str(day[0]) in date_list:
+            companies[company][1].append(int(day[2]))
+print(companies)
